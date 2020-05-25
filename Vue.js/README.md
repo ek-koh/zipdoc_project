a0c21a459d50ae1236573a13c4089943a72b8378
e-koh <ekherb1244@gmail.com>
2020-05-25 12:54:22 +0900

    CoreUI

---
+++ Vue.js/README.md
@@ -0,0 +1,18 @@
+# vue.js_zipdoc
+
+> zipdoc project via Vue.js visualization
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ Vue.js/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <div ref="tableau" class="center"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello Tableau Fans!',
+      url: "http://public.tableau.com/views/__15868688937150/1_2",
+      options: {
+        hideTabs: true
+      }
+    }
+  },
+  methods: {
+    initViz: function () {
+      let viz = new tableau.Viz(this.$refs.tableau, this.url, this.options);
+    }
+  },
+  mounted: function() {
+    this.initViz()
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
